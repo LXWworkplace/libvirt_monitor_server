46939de8b5e3e56ea5d06281ed7fd1c56dc06e22
songboyu <soongboyu@163.com>
2015-11-15 16:42:41 +0800

    complete

--- code/cloud_monitor_settings.py
+++ code/cloud_monitor_settings.py
@@ -1,14 +1,14 @@
 # configuration for cloud_monitor and etc...
-# db_engine = 'mysql'
-# db_server = '173.26.100.209'
-# db_username = 'root'
-# db_password ='123456'
-# db_database = 'cloud_monitor'
 db_engine = 'mysql'
-db_server = 'localhost'
+db_server = '173.26.100.209'
 db_username = 'root'
 db_password ='123456'
 db_database = 'cloud_monitor'
+# db_engine = 'mysql'
+# db_server = 'localhost'
+# db_username = 'root'
+# db_password ='123456'
+# db_database = 'cloud_monitor'
 
 api_server_port = 9898
 
